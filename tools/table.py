535c1f09319c8110269c6932083a1e1e8d0ed666
Tom Seddon <tom@tmbp>
2020-09-27 22:35:50 +0100

    Sort out various tables.

--- tools/table.py
+++ tools/table.py
@@ -12,14 +12,21 @@ def fatal(msg):
 ##########################################################################
 
 def table(options):
-    with open('mos.rom','rb') as f: mos=[ord(x) for x in f.read()]
-
+    orig_mos=os.path.join(os.path.dirname(sys.argv[0]),
+                          '../orig/MOS320.rom')
+    with open(orig_mos,'rb') as f:
+        mos=f.read(16384)
+        f.seek(7*16384)
+        terminal=f.read(16384)
+        mos=[ord(x) for x in terminal+mos]
+        assert len(mos)==32768
+    
     if options.word:
         size=2
-        directive='EQUW'
+        directive='.word'
     else:
         size=1
-        directive='EQUB'
+        directive='.byte'
         
     if (options.last-options.first+1)%size!=0:
         fatal('length of table must be a multiple of %d'%size)
@@ -27,22 +34,22 @@ def table(options):
     for addr in range(options.first,options.last+1,size):
         value=0
 
-        for i in range(size): value|=mos[(addr+i)&0x3fff]<<(i*8)
+        for i in range(size): value|=mos[(addr+i)&0x7fff]<<(i*8)
 
-        line='%s &%0*X'%(directive,size*2,value)
+        line='%s $%0*X'%(directive,size*2,value)
 
         # line='EQUB &%02X'%value
 
         while len(line)<17: line+=' '
 
-        line+=':\ %04X='%addr
+        line+='; %04X='%addr
 
-        for i in range(size): line+=' %02X'%mos[(addr+i)&0x3fff]
+        for i in range(size): line+=' %02X'%mos[(addr+i)&0x7fff]
 
         while len(line)<38: line+=' '
 
         for i in range(size):
-            value=mos[(addr+i)&0x3fff]
+            value=mos[(addr+i)&0x7fff]
             if value>=32 and value<=127: line+=chr(value)
             else: line+='.'
         
